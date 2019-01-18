c34c3b94f647620c178495a1705fa9be6a6c171d
subarin38 <ygrassano@hotmail.fr>
2019-01-18 18:15:18 +0100

    modification du labyrinthe.py pour qu'il commence à fonctionner

--- labyrinthe.py
+++ labyrinthe.py
@@ -7,55 +7,22 @@
 
 import pygame
 
-class Labyrinthe ():
+from pygame.locals import *
+from constantes import *
+from classes import *
 
+def main():
 
-	Labyrinthe = ["D", "s", "s", "s", "s", "m", "m", "m", "m", "m", "m", "s", "s", "s", "s",
-                "m", "m", "m", "m", "s", "m", "m", "m", "m", "m", "m", "s", "m", "m", "m", 
-                "m", "s", "s", "s", "s", "s", "s", "s", "s", "s", "s", "s", "m", "m", "m",
-                "m", "s", "m", "m", "m", "m", "m", "m", "m", "m", "m", "s", "m", "s", "m", 
-                "m", "s", "m", "m", "m", "m", "s", "m", "m", "m", "m", "s", "m", "s", "m", 
-                # à continuer c'est juste pour voir si c'est la bonne méthode + j'ai trop d'indentation il me semble
-
-    Blocs = {"sol": "s", "mur": "m", "départ": "D", "fin": "f"}
-
-    def __init__(self):
-        pass
-
-# Généraion de la list pour le labyrinthe avec les images
-@property
-def show_Labyrinthe(sefl):
-    # mettre ici les variables pour les images correspondant aux items de la liste
-
-
-=======
-#!/usr/bin/python3
-# -*- coding: Utf-8 -*
-
-# fichier cherchant à contenir le labyrinthe du projet macgyver
-# Créer les cellules du labyrinthe
-
-import pygame
-
-class Labyrinthe ():
-
-
-	Labyrinthe = ["D", "s", "s", "s", "s", "m", "m", "m", "m", "m", "m", "s", "s", "s", "s",
-                  "m", "m", "m", "m", "s", "m", "m", "m", "m", "m", "m", "s", "m", "m", "m", 
-                  "m", "s", "s", "s", "s", "s", "s", "s", "s", "s", "s", "s", "m", "m", "m",
-                  "m", "s", "m", "m", "m", "m", "m", "m", "m", "m", "m", "s", "m", "s", "m", 
-                  "m", "s", "m", "m", "m", "m", "s", "m", "m", "m", "m", "s", "m", "s", "m", 
-                  # à continuer c'est juste pour voir si c'est la bonne méthode + j'ai trop d'indentation il me semble
-
-    Blocs = {"sol": "s", "mur": "m", "départ": "D", "fin": "f"}
-
-    def __init__(self):
-        pass
-
-# Généraion de la list pour le labyrinthe avec les images
-@property
-def show_Labyrinthe(sefl):
-    # mettre ici les variables pour les images correspondant aux items de la liste
-
-
->>>>>>> 84d342ed719372c7eb3947a242d6db262563a7aa
+	pygame.init()
+	
+	window = pygame.display.set_mode((COTE_WINDOW, COTE_WINDOW)
+	
+	
+	GARDIEN = pygame.image.load("images/Gardien.png").convert()
+	MACGYVER = pygame.image.load("images/MacGyver.png").convert()
+	SOL = pygame.image.load("images/sol.png").convert()
+	WALL = pygame.image.load("images/wall.png").convert()
+	#Pourquoi certains utilisent convert() et d'autres convert_alpha() ??
+	
+	ARRAY = []
+	
