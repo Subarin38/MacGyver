921265287049db1791697d4ae380442907c61f84
Subarin38 <45593361+Subarin38@users.noreply.github.com>
2019-01-26 10:43:05 +0100

    Update constantes.py
    Ajout des constantes de fichiers images

--- constantes.py
+++ constantes.py
@@ -4,3 +4,8 @@
 NOMBRE_SPRITE_COTE = 15
 TAILLE_SPRITE = 45
 COTE_WINDOW = NOMBRE_SPRITE_COTE * TAILLE_SPRITE
+
+WALL = pygame.image.load("images/wall.png").convert()
+SOL = pygame.image.load("images/sol.png").convert()
+GARDIEN = pygame.image.load("images/gardien.png").convert()
+MACGYVER = pygame.image.load("images/macgyver.png").convert()
