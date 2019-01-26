2c007e104c4363ae64d6d1f991609e83db25af39
Subarin38 <45593361+Subarin38@users.noreply.github.com>
2019-01-26 10:42:27 +0100

    Changement du labyrinthe.py en main.py

--- main.py
+++ main.py
@@ -5,10 +5,13 @@
 # Créer les cellules du labyrinthe
 
 import pygame
+import class.py
 
 from pygame.locals import *
 from constantes import *
 from classes import *
+
+pygame.init()
 
 def main():
 
@@ -17,10 +20,7 @@ def main():
 	window = pygame.display.set_mode((COTE_WINDOW, COTE_WINDOW)
 	
 	
-	GARDIEN = pygame.image.load("images/Gardien.png").convert()
-	MACGYVER = pygame.image.load("images/MacGyver.png").convert()
-	SOL = pygame.image.load("images/sol.png").convert()
-	WALL = pygame.image.load("images/wall.png").convert()
+
 	#Pourquoi certains utilisent convert() et d'autres convert_alpha() ??
 	
 	ARRAY = []
