69a3b2c70d40eb0c4655dca4ebe8f16b61236a08
Subarin38 <ygrassano@hotmail.fr>
2019-02-07 19:28:53 +0100

    première modification de séance

--- constantes.py
+++ constantes.py
@@ -4,13 +4,13 @@ import pygame
 pygame.display.init() 
 pygame.display.set_mode()
 #Paramètres de la fenêtre
-NOMBRE_SPRITE_COTE = 30
-TAILLE_SPRITE = 15
+NOMBRE_SPRITE_COTE = 15
+TAILLE_SPRITE = 32
 COTE_WINDOW = NOMBRE_SPRITE_COTE * TAILLE_SPRITE
 
 WALL = pygame.image.load("images/wall.png").convert()
 SOL = pygame.image.load("images/sol.png").convert()
 GARDIEN = pygame.image.load("images/gardien.png").convert()
 MACGYVER = pygame.image.load("images/macgyver.png").convert()
-ACCUEIL = pygame.image.load("images/accueil.png").convert()
+
 
--- test.py
+++ test.py
@@ -20,55 +20,35 @@ window = pygame.display.set_mode((COTE_WINDOW,COTE_WINDOW))
 
 My_labyrinthe = labyrinthe()
 
-continuer = 1
-while continuer:
+continuer_jeu = 1
+			
+My_labyrinthe.creation()	
+My_labyrinthe.afficher(window)
+pygame.display.flip()
+mg = macgyver
+
+while continuer_jeu:
+	for event in pygame.event.get():
 	
-	window.blit(ACCUEIL, (0,0))
-	pygame.display.flip()
-
-	continuer_accueil = 1
-	continuer_jeu = 1
-
-	while continuer_accueil:	
-		for event in pygame.event.get():
-			if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-				continuer_accueil = 0
+		if event.type == QUIT:
+			continuer_jeu = 0
+			continuer = 0
+	
+		elif event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
 				continuer_jeu = 0
-				continuer = 0
-				choix = 0
-				
-			elif event.type == KEYDOWN:				
-				if event.key == K_F5:
-					continuer_accueil = 0	
-					choix = 'Map1.txt'		
-	if choix != 0:		
-		My_labyrinthe.creation()	
-		My_labyrinthe.afficher(window)
-		pygame.display.flip()
-		mg = macgyver
 
-	while continuer_jeu:
-		for event in pygame.event.get():
-		
-			if event.type == QUIT:
-				continuer_jeu = 0
-				continuer = 0
-		
-			elif event.type == KEYDOWN:
-				if event.key == K_ESCAPE:
-					continuer_jeu = 0
+			elif event.key == K_RIGHT:
+				mg.déplacements('droite')
+			elif event.key == K_LEFT:
+				mg.déplacements('gauche')
+			elif event.key == K_UP:
+				mg.déplacements('haut')
+			elif event.key == K_DOWN:
+				mg.déplacements('bas')	
 
-				elif event.key == K_RIGHT:
-					mg.déplacements('droite')
-				elif event.key == K_LEFT:
-					mg.déplacements('gauche')
-				elif event.key == K_UP:
-					mg.déplacements('haut')
-				elif event.key == K_DOWN:
-					mg.déplacements('bas')	
-
-		if My_labyrinthe.structure[mg.case_x][mg.case.y] == "F":
-			continuer_jeu = 0 
+		### if My_labyrinthe.structure[mg.case_x][mg.case.y] == "F":
+			## continuer_jeu = 0 
 
 
 
